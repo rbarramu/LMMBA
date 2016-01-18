4c0b77447fea36d2333d02f8bb7af2dbf055ec6a
pablodg12 <pabloibarra@pablos-MacBook-Pro.local>
2016-01-17 23:38:16 -0300

    Se agrego atributo para diferencias tipo dea rchivo

--- grails-app/domain/lmmba/Document.groovy
+++ grails-app/domain/lmmba/Document.groovy
@@ -4,6 +4,8 @@ class Document {
     String filename
     String fullPath
     Date uploadDate = new Date()
+    String descripcion
+    String tipo
     static constraints = {
         filename(blank:false,nullable:false)
         fullPath(blank:false,nullable:false)
